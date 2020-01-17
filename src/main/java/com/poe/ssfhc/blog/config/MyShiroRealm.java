131e8fb8c2fb1320cc5437f3fdea8119cd3f84ef
Vince <527687529@qq.com>
2020-01-17 15:28:04 +0800

    fix: 补充shiro配置

---
+++ src/main/java/com/poe/ssfhc/blog/config/MyShiroRealm.java
@@ -0,0 +1,83 @@
+package com.poe.ssfhc.blog.config;
+
+
+import com.poe.ssfhc.blog.authority.dao.MenuMapper;
+import com.poe.ssfhc.blog.authority.dao.RoleMapper;
+import com.poe.ssfhc.blog.authority.dao.UserMapper;
+import com.poe.ssfhc.blog.authority.domain.Menu;
+import com.poe.ssfhc.blog.authority.domain.Role;
+import com.poe.ssfhc.blog.authority.domain.User;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @ClassName: MyShiroRealm
+ * @Description: TODO
+ * @Author: Vicne
+ * @Date: 2020/1/17 14:30
+ * @Version: v1.0
+ */
+public class MyShiroRealm extends AuthorizingRealm {
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private RoleMapper roleMapper;
+    @Autowired
+    private MenuMapper menuMapper;
+
+    /**
+     * 授权
+     *
+     * @param principalCollection 身份集合
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        User user = (User) principalCollection.getPrimaryPrincipal();
+        List<Role> roles = roleMapper.selectByPrimaryKey(user.getUserId());
+        List<Menu> menus = menuMapper.selectByPrimaryKey(user.getUserId());
+        simpleAuthorizationInfo.addRoles(roles.stream().map(Role::getRoleName).collect(Collectors.toSet()));
+        simpleAuthorizationInfo.addStringPermissions(menus.stream().map(Menu::getPerms).collect(Collectors.toSet()));
+        return simpleAuthorizationInfo;
+    }
+
+    /**
+     * 认证
+     *
+     * @param authenticationToken token
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String username = (String) authenticationToken.getPrincipal();
+        User user = userMapper.getUserByUsername(username);
+        if (user == null) {
+            throw new UnknownAccountException("用户名或密码错误");
+        }
+        SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(
+                user, user.getPassword(), ByteSource.Util.bytes(user.getSalt()), getName()
+        );
+        Session session = SecurityUtils.getSubject().getSession();
+        session.setAttribute("user", user);
+        return simpleAuthenticationInfo;
+    }
+
+    /**
+     * 清理缓存权限
+     */
+    public void clearCachedAuthorizationInfo() {
+        this.clearCachedAuthorizationInfo(SecurityUtils.getSubject().getPrincipals());
+    }
+}
---
+++ src/main/java/com/poe/ssfhc/blog/config/ShiroConfig.java
@@ -0,0 +1,112 @@
+package com.poe.ssfhc.blog.config;
+
+
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @ClassName: ShiroConfig
+ * @Description: TODO
+ * @Author: Vicne
+ * @Date: 2020/1/17 14:30
+ * @Version: v1.0
+ */
+@Configuration
+public class ShiroConfig {
+    public static final String LOGIN_URL="/login";
+    public static final String SUCCESS_URL="/index";
+
+    /**
+     * 管理bean生命周期
+     * @return
+     */
+    @Bean
+    public static LifecycleBeanPostProcessor getLifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager){
+        ShiroFilterFactoryBean shiroFilterFactoryBean=new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        shiroFilterFactoryBean.setLoginUrl(LOGIN_URL);
+        shiroFilterFactoryBean.setSuccessUrl(SUCCESS_URL);
+
+        Map<String,String> filterChainDefinitionMap=new LinkedHashMap<>();
+        //放行swagger(下面6行)
+        filterChainDefinitionMap.put("/swagger-ui.html","anon");
+        filterChainDefinitionMap.put("/swagger/**","anon");
+        filterChainDefinitionMap.put("/webjars/**", "anon");
+        filterChainDefinitionMap.put("/swagger-resources/**","anon");
+        filterChainDefinitionMap.put("/v2/**","anon");
+        filterChainDefinitionMap.put("/static/**", "anon");
+
+        filterChainDefinitionMap.put("logout","logout");
+        filterChainDefinitionMap.put("/login","anon");
+        filterChainDefinitionMap.put("/**","authc");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    public SecurityManager securityManager(){
+        DefaultWebSecurityManager securityManager =  new DefaultWebSecurityManager();
+        securityManager.setRealm(myShiroRealm());
+        return  securityManager;
+    }
+
+    @Bean
+    public MyShiroRealm myShiroRealm(){
+        MyShiroRealm myShiroRealm=new MyShiroRealm();
+        myShiroRealm.setCredentialsMatcher(hashedCredentialsMatcher());
+        return myShiroRealm;
+    }
+
+    /**
+     * 凭证匹配，加密算法
+     * @return
+     */
+    @Bean
+    public HashedCredentialsMatcher hashedCredentialsMatcher(){
+        HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
+        hashedCredentialsMatcher.setHashAlgorithmName("md5");
+        hashedCredentialsMatcher.setHashIterations(2);
+        return hashedCredentialsMatcher;
+    }
+
+    /**
+     * 开启shiro 注解支持
+     * @param securityManager
+     * @return
+     */
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager){
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+        return authorizationAttributeSourceAdvisor;
+    }
+
+    /**
+     * 开启shiro授权注解，若上面Bean未生效则使用此Bean
+     * @return
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator(){
+        DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator=new DefaultAdvisorAutoProxyCreator();
+        advisorAutoProxyCreator.setProxyTargetClass(true);
+        return advisorAutoProxyCreator;
+    }
+
+}
